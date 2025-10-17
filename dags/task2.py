726484ff82ec1f5936462ffe1edd154f273972e9
Rajeswari <rajeswarivaithi@gmail.com>
2025-10-17 17:37:25 +0530

    Initial commit for Astro deployment13

---
+++ dags/task2.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from airflow import DAG
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from airflow.operators.python import PythonOperator
+from datetime import timedelta
+import pendulum
+import io
+import logging
+
+
+# Default arguments for the DAG
+default_args = {
+   'owner': 'airflow',
+   'start_date': pendulum.datetime(2025, 1, 1, tz="UTC")
+,
+   'retries': 1,
+}
+
+
+# Define the DAG
+dag = DAG(
+   's3_read_success_file',
+   default_args=default_args,
+   description='DAG to read daily files',
+   schedule=None, # Set to None for manual execution
+   catchup=False,
+   max_active_runs=1,
+   depends_on_past=True
+)
+
+
+# AWS S3 connection details
+BUCKET_NAME = 'airflowbuckets25'
+OBJECT_KEY1 = 'daily_files/SUCCESS_20251017'
+#LOCAL_FILE_PATH = '/usr/local/airflow/include/'
+
+wait_for_file1 = S3KeySensor(
+   task_id='wait_for_success_file',
+   bucket_name=BUCKET_NAME,
+   bucket_key=OBJECT_KEY1,
+   aws_conn_id='aws_s3', # Use your AWS connection ID
+   poke_interval=60, # How often to check the S3 bucket (in seconds)
+   timeout=600, # How long to wait before giving up (in seconds)
+   mode='poke', # This mode will keep checking until the file is found
+   dag=dag,
+)
+
+def process_data(**kwargs):
+    print("processing data for the day")
+
+process_data_task = PythonOperator(
+    task_id='process_data',
+    python_callable=process_data,
+    execution_timeout=timedelta(hours=2),
+)
+
+wait_for_file1 >> process_data_task
